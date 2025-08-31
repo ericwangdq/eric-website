257f4c623df2a6e66d569e3705a524b063f634fc
Eric Wang <eric.wang04@sap.com>
2025-09-01 00:10:04 +0800

    change edit url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -45,7 +45,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/ericwangdq/eric-website",
+          editUrl: "https://github.com/ericwangdq/eric-website/blob/main/",
         },
         blog: {
           showReadingTime: true,
@@ -55,7 +55,7 @@ const config: Config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/ericwangdq/eric-website",
+          editUrl: "https://github.com/ericwangdq/eric-website/blob/main/",
           // Useful options to enforce blogging best practices
           onInlineTags: "warn",
           onInlineAuthors: "warn",
